00969d4da88f1cd47fe9f6aadd043f5fe10b1fc9
prasad <prasadpatil91640@gmail.com>
2024-12-31 17:32:44 +0530

    completed the component structure

--- app.vue
+++ app.vue
@@ -5,8 +5,46 @@
         <img src="https://themewagon.gitlab.io/restaurant/assets/img/logo.png" alt="logo">
       </div>
       <ul class="nav-links flex space-x-8 mr-80">
-        <li><a href="#" class="text-white text-opacity-70">Home</a></li>
-        <li><a href="#" class="text-white text-opacity-70">Pages</a></li>
+        <div class="flex items-center space-x-6">
+        <div class="relative group">
+          <li class="text-gray-400 transition-colors cursor-pointer hover:text-white">
+            Home
+          </li>
+          <div class="absolute left-0 hidden pt-4 group-hover:block">
+            <div class="w-48 bg-black shadow-lg py-2">
+              <a href="#" class="block px-4 py-2 text-gray-400 hover:text-white font-mono text-sm"
+                >Header Static</a>
+              <a href="#" class="block px-4 py-2 text-gray-400 hover:text-white font-mono text-sm"
+                >Header Slider</a>
+              <a href="#" class="block px-4 py-2 text-gray-400 hover:text-white font-mono text-sm"
+                >Youtube Background</a>
+              <a href="#" class="block px-4 py-2 text-gray-400 hover:text-white font-mono text-sm"
+                >Self Hosted Video</a>
+              <a href="#" class="block px-4 py-2 text-gray-400 hover:text-white font-mono text-sm"
+                >RTL</a>
+            </div>
+          </div>
+        </div>
+        </div>
+        <div class="flex items-center space-x-6">
+        <div class="relative group">
+          <li class="text-gray-400 transition-colors cursor-pointer hover:text-white">
+            Pages
+          </li>
+          <div class="absolute left-0 hidden pt-4 group-hover:block">
+            <div class="w-48 bg-black shadow-lg py-2">
+              <a href="#" class="block px-4 py-1 text-gray-400 hover:text-white font-mono text-sm"
+                >Menu</a>
+              <a href="#" class="block px-4 py-1 text-gray-400 hover:text-white font-mono text-sm"
+                >Gallery</a>
+              <a href="#" class="block px-4 py-1 text-gray-400 hover:text-white font-mono text-sm"
+                >About</a>
+              <a href="#" class="block px-4 py-1 text-gray-400 hover:text-white font-mono text-sm"
+                >Contact</a>
+            </div>
+          </div>
+        </div>
+        </div>
         <li><a href="#" class="text-white text-opacity-70">Menu</a></li>
         <li><a href="#" class="text-white text-opacity-70">Components</a></li>
         <li><a href="#" class="text-white text-opacity-70">Utilities</a></li>
@@ -16,9 +54,10 @@
         <p>MAKE A RESERVATION</p>
       </button>
     </nav>
-    <section class="home">
-      <aside class="leftaside relative mt-[130px] ml-[10%] ml-[80px]">
+    <section class="home mt-[-60px]" style="background: linear-gradient(to bottom, rgba(255, 255, 0, 0.5), rgba(255, 255, 0, 0));">
+      <aside class="leftaside relative pt-[190px] ml-[10%] ml-[80px]">
         <img src="https://themewagon.gitlab.io/restaurant/assets/img/food/plate.png" alt="img" height="335" width="335">
+        
       </aside>
       <aside class="rightaside relative md:text-left mt-[-490px] lg:ml-[480px]">
         <div class="heading1 pt-56 pl-5 text-4xl font-serif">
@@ -76,7 +115,7 @@
           <div class="p-4 bg-gray-50 border-r-2 border-gray-300 w-[320px] h-150">
             <a class="text-decoration-none " href="#">
               <div class="flex justify-between">
-                
+
                 <h2 class="font-bold text-black text-xl font-mono">BREAKFAST BOWL</h2>
                 <p class="font-bold text-red-600 text-opacity-70">$8.50</p>
               </div>
@@ -169,7 +208,6 @@
           </div>
         </div>
       </div>
-      
     </div>
     
   </div>
@@ -286,21 +324,158 @@
         <img src="https://themewagon.gitlab.io/restaurant/assets/img/food/soup.png" alt="soup">
       </div>
     </section>
-    <section style="background-image: url('	https://themewagon.gitlab.io/restaurant/assets/img/food/green.png'); background-repeat: no-repeat; b"></section>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <section style="background-image: url('https://themewagon.gitlab.io/restaurant/assets/img/food/green.png'); background-repeat: no-repeat; height: 700px;" class="bg-gray-50 h-[700px] w-full">
+      <div><center><p class="font-mono text-2xl pt-[120px] text-opacity-70" >Location & Timings</p></center></div>
+      <div><center><h2 class="font-serif font-bold pt-2 text-4xl">Contact with us</h2></center></div>
+      <br><br><br>
+      <div class="flex justify-between items-center pt-12 h-80 w-full flex px-[80px]">
+        <div class="h-[230px] w-[350px] bg-white rounded-md justify-center">
+          <p class="font-mono text-md text-gray-500 pl-[50px] pt-[50px]">Av. Paz Soldán 290, San <br>Isidro, Lima 27 - Peru</p><br>
+          <p class="font-mono text-sm text-gray-500 pl-[50px]">+511 442-2777</p><br>
+          <p class="font-mono text-sm text-gray-500 pl-[50px]">mail@restaurantpro.com</p>
+        </div>
+        <div class="h-[230px] w-[350px] bg-white rounded-md">
+          <p class="font-bold text-red-600 text-opacity-70 pl-[50px] pt-[20px]">Hours of service</p>
+          <p class="font-medium font-sans text-md pl-[50px]">Monday to Saturday</p>
+          <p class="font-mono text-md text-gray-500 pl-[50px]">1:00 pm - 3:00 pm and <br>7:00 pm - 11:00 pm</p><br>
+          <p class="font-medium font-sans text-md pl-[50px]">Sunday</p>
+          <p class="font-mono text-md text-gray-500 pl-[50px]">12:30 pm - 3:30 pm</p>
+        </div>
+        <div class="h-[230px] w-[350px] bg-white rounded-md" style="background-image: url('https://themewagon.gitlab.io/restaurant/assets/img/food/kisspng-soup.png');">
+          <p class="font-bold text-gray-700 font-sans text-md pl-[50px] pt-[50px]">Telephone support:</p>
+          <p class="font-sans text-gray-600 text-md pl-[50px]">Monday to Saturday from <br>9:00 am to 9:00 pm</p>
+          <br><button class="bg-red-400 text-gray-800 ml-[50px] py-2 px-10 rounded-md cursor-pointer font-bold hover:bg-red-500 font-sans text-opacity-70">
+            MAKE A RESERVATION</button>
+        </div>
+      </div>
+      
+    </section>
+    <section class="h-[420px] w-full bg-gray-300">
+      <center>
+        <div class="pt-[140px]">
+          <img src="https://maps.gstatic.com/mapfiles/api-3/images/icon_error.png" alt="">
+        </div>
+        <div class="pt-2">
+          <h1 class="text-2xl font-sans">Oops! Something went wrong.</h1><br>
+          <p class="text-sm font-sans">This page didn't load Google Maps correctly. See the JavaScript console for technical details.</p>
+        </div>
+      </center>
+    </section>
+    <section class="h-[600px] w-full">
+      
+  <div class="w-full px-6 py-8 bg-white shadow-lg rounded-md bg-gray-100">
+    <h1 class="text-3xl font-medium font-serif text-center mb-4 mt-[80px]">Feel free to drop us a line!</h1>
+    <p class="text-sm text-center font-mono text-gray-600 mb-6">
+      Let's talk if you have any query or suggestion. We are open to learn from you. <br>So don't hesitate to reach us anytime.
+    </p>
+    <form class="flex pt-[50px]">
+      <div class="h-[300px] w-[630px]">
+        <div class="mb-4">
+        <input
+          type="text"
+          id="name"
+          class="w-full px-4 py-2 font-mono border border-gray-300 rounded-md focus:ring-2 focus:ring-yellow-300 focus:outline-none"
+          placeholder="Your Name"
+        />
+      </div>
+      <div class="mb-4">
+        
+        <input
+          type="text"
+          id="subject"
+          class="w-full px-4 py-2 font-mono border border-gray-300 rounded-md focus:ring-2 focus:ring-yellow-300 focus:outline-none"
+          placeholder="Subject"
+        />
+      </div>
+      <div class="mb-4">
+        <input
+          type="email"
+          id="email"
+          class="w-full px-4 py-2 font-mono border border-gray-300 rounded-md focus:ring-2 focus:ring-yellow-300 focus:outline-none"
+          placeholder="Email Address"
+        />
+      </div>
+      <div class="mb-4 flex items-center">
+        <input
+          type="checkbox"
+          id="copy"
+          class="h-4 w-4 accent-yellow-400 border-gray-300 rounded focus:ring-yellow-300"
+        />
+        <label for="copy" class="ml-2 font-serif text-md text-gray-700">Send me a copy</label>
+      </div>
+
+      </div>
+      <div class="h-[300px] w-[630px]">
+        <div class="mb-4 ml-4">
+        <textarea
+          id="message"
+          class="w-full px-4 py-2 font-mono border border-gray-300 rounded-md focus:ring-2 focus:ring-yellow-300 focus:outline-none"
+          placeholder="Type your message here"
+          rows="4"
+        ></textarea>
+      </div>
+      <button class="bg-gray-900 text-gray-100 ml-[20px] py-2 px-10 rounded-md cursor-pointer font-medium hover:bg-black font-sans text-opacity-70">
+        GIVE US A SHOT</button>
+    </div>
+    </form>
+  </div>
+
+
+  </section>
+
+    <br><br><br>
+    <section class="h-[350px] w-full flex">
+      <div class="h-[300px] w-[600px] ml-[320px] justify-center space-y-2">
+        <center><p class="font-mono text-gray-500 mt-[100px]">"Restaurant & Cafe, we aim at promoting the foodstuff <br>industry through the branches , we establish and <br>through the new dishes. and integrate between different <br>cultures, by merging different Oriental and its western <br>recipes"</p></center>
+        <center><p class="font-mono text-xl font-bold text-gray-700">-Lily Rose</p></center>
+      </div>
+      <div class="h-[300px] w-[320px] ml-[80px] mb-[60px]">
+        <img src="https://themewagon.gitlab.io/restaurant/assets/img/food/pizza2.png" alt="" class="h-70 w-[300px]">
+      </div>
+    </section>
+
+
+
+    <section class="h-[320px] w-full bg-black flex px-[140px]">
+      <div class="h-[320px] w-[180px]">
+        <img src="https://themewagon.gitlab.io/restaurant/assets/img/food/pngfood2.png" alt="Noodles" class="h-[260px] ml-[20px] mt-[60px]">
+      </div>
+      <div class="h-[320px] w-[300px]">
+        <img src="https://themewagon.gitlab.io/restaurant/assets/img/logo.png" alt="logo" class="pt-[80px] "><br>
+        <h1 class="text-3xl text-white font-medium font-serif">Want To Taste <br>Our Food?</h1>
+        <br>
+        <button class="bg-yellow-400 text-gray-800 py-2 px-10 rounded-md cursor-pointer font-medium hover:bg-yellow-500 font-sans text-opacity-70">
+          ORDER ONLINE</button>
+      </div>
+      <div class="h-[320px] w-[200px] ml-[70px] pl-[60px]">
+        <h3 class="mt-[60px] text-white font-bold font-mono text-xl">ABOUT</h3>
+        <ul class="mt-[30px] space-y-2 text-gray-500 text-sm ">
+          <li class="hover:text-gray-700"><a href="#">About</a></li>
+          <li class="hover:text-gray-700"><a href="#">Services</a></li>
+          <li class="hover:text-gray-700"><a href="#">careers</a></li>
+          <li class="hover:text-gray-700"><a href="#">Hire us</a></li>
+        </ul>
+      </div>
+      <div class="h-[320px] w-[250px] ">
+        <h3 class="mt-[60px] text-white font-bold font-mono text-xl">RESOURCES</h3>
+        <ul class="mt-[30px] space-y-2 text-gray-500 text-sm ">
+          <li class="hover:text-gray-700"><a href="#">Term</a></li>
+          <li class="hover:text-gray-700"><a href="#">Help</a></li>
+          <li class="hover:text-gray-700"><a href="#">Privacy</a></li>
+        </ul>
+        <div class="flex space-x-4 mt-6 h-[30px]">
+          
+        </div>
+      </div>
+    </section>
   </div>
 
 
 </template>
 
 <style scoped>
-#btn{
-  cursor: pointer;
-}
-#aside{
-  height: 200px;
-  width: 200px;
-  background-color: aqua;
-}
+
 </style>
 <script setup>
   import { ref } from 'vue';
